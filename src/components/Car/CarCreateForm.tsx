4221b884384208d5839ea6241273e45dde8d94fd
Hridoy Islam <hridoy4t@gmail.com>
2024-04-02 19:29:41 +0600

    login works

--- src/components/Car/CarCreateForm.tsx
+++ src/components/Car/CarCreateForm.tsx
@@ -1,89 +1,182 @@
-import CheckboxFive from '../Checkboxes/CheckboxFive';
-import CheckboxFour from '../Checkboxes/CheckboxFour';
-import CheckboxOne from '../Checkboxes/CheckboxOne';
-import CheckboxThree from '../Checkboxes/CheckboxThree';
-import CheckboxTwo from '../Checkboxes/CheckboxTwo';
-import DatePickerOne from '../Forms/DatePicker/DatePickerOne';
-import DatePickerTwo from '../Forms/DatePicker/DatePickerTwo';
-import MultiSelect from '../Forms/MultiSelect';
 import SelectGroupThree from '../Forms/SelectGroup/SelectGroupThree';
-import SwitcherFour from '../Switchers/SwitcherFour';
-import SwitcherOne from '../Switchers/SwitcherOne';
-import SwitcherThree from '../Switchers/SwitcherThree';
-import SwitcherTwo from '../Switchers/SwitcherTwo';
 
 const CarCreateForm = () => {
   return (
-    <div className="grid grid-cols-1 ">
+    <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
       <div className="flex flex-col gap-9">
         {/* <!-- Input Fields --> */}
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
           <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
             <h3 className="font-medium text-black dark:text-white">
-              Input Fields
+              Car Details
             </h3>
           </div>
           <div className="flex flex-col gap-5.5 p-6.5">
             <div>
               <label className="mb-3 block text-black dark:text-white">
-               Title
+                Car Name
               </label>
               <input
                 type="text"
-                placeholder="Enter Title "
+                placeholder="Car Name"
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
             </div>
 
             <div>
               <label className="mb-3 block text-black dark:text-white">
-               Model
+                Car Brand
               </label>
               <input
                 type="text"
-                placeholder="Enter Model "
-                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-              />
-            </div>
-
-
-            <div>
-              <label className="mb-3 block text-black dark:text-white">
-               Brand
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Brand "
+                placeholder="Brand"
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
             </div>
 
             <div>
-              
-              <SelectGroupThree />
+              <label className="mb-3 block text-black dark:text-white">
+                Color
+              </label>
+              <input
+                type="text"
+                placeholder="Color "
+                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
             </div>
 
             <div>
               <label className="mb-3 block text-black dark:text-white">
-             Description
+                Model
               </label>
-              <textarea
-                rows={6}
-                placeholder="Description"
+              <input
+                placeholder="Model"
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-              ></textarea>
+              />
             </div>
-            
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Registration Year
+              </label>
+              <input
+                placeholder="Registration Year"
+                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Production Year
+              </label>
+              <input
+                placeholder="Registration Year"
+                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
           </div>
         </div>
-
-
-      
-
-      
       </div>
+      <div className="flex flex-col gap-9">
+        {/* <!-- Input Fields --> */}
+        <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+          <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+            <h3 className="font-medium text-black dark:text-white">
+              Other Details
+            </h3>
+          </div>
+          <div className="flex flex-col gap-5.5 p-6.5">
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Condition
+              </label>
+              <select>
+                <option value="">Used</option>
+                <option value="">Recondition</option>
+              </select>
+            </div>
 
-  
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Transmition
+              </label>
+              <select>
+                <option value="">Automatic</option>
+                <option value="">Manual</option>
+              </select>
+            </div>
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Fuel Type
+              </label>
+              <input
+                type="text"
+                className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Engine Capacity
+              </label>
+              <input
+                type="text"
+                className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Kilometer
+              </label>
+              <input
+                type="text"
+                className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Details
+              </label>
+              <textarea className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"></textarea>
+            </div>
+
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Price
+              </label>
+              <input className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary" />
+            </div>
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Phone
+              </label>
+              <input className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary" />
+            </div>
+
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                District
+              </label>
+              <input className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary" />
+            </div>
+
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Upazila
+              </label>
+              <input className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary" />
+            </div>
+
+            <div>Upload Car Images</div>
+
+            <div>
+              <input
+                type="submit"
+                placeholder="Create Driver"
+                className="rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Driver/DriverCreate.tsx
+++ src/components/Driver/DriverCreate.tsx
@@ -1,5 +1,3 @@
-import SelectGroupThree from '../Forms/SelectGroup/SelectGroupThree';
-
 const DriverCreateForm = () => {
   return (
     <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
@@ -8,56 +6,51 @@ const DriverCreateForm = () => {
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
           <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
             <h3 className="font-medium text-black dark:text-white">
-              Input Fields
+              Personal Information
             </h3>
           </div>
           <div className="flex flex-col gap-5.5 p-6.5">
             <div>
               <label className="mb-3 block text-black dark:text-white">
-                Title
+                Drivers Name
               </label>
               <input
                 type="text"
-                placeholder="Enter Title "
+                placeholder="name"
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
             </div>
 
             <div>
               <label className="mb-3 block text-black dark:text-white">
-                Model
+                Drivers Phone
               </label>
               <input
                 type="text"
-                placeholder="Enter Model "
+                placeholder="Phone"
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
             </div>
 
             <div>
               <label className="mb-3 block text-black dark:text-white">
-                Brand
+                District
               </label>
               <input
                 type="text"
-                placeholder="Enter Brand "
+                placeholder="Enter District "
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
             </div>
 
             <div>
-              <SelectGroupThree />
-            </div>
-
-            <div>
               <label className="mb-3 block text-black dark:text-white">
-                Description
+                Upazila
               </label>
-              <textarea
-                rows={6}
-                placeholder="Description"
+              <input
+                placeholder="Upazila"
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-              ></textarea>
+              />
             </div>
           </div>
         </div>
@@ -67,56 +60,65 @@ const DriverCreateForm = () => {
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
           <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
             <h3 className="font-medium text-black dark:text-white">
-              Input Fields
+              Other Details
             </h3>
           </div>
           <div className="flex flex-col gap-5.5 p-6.5">
             <div>
               <label className="mb-3 block text-black dark:text-white">
-                Title
+                Car Name
               </label>
               <input
                 type="text"
-                placeholder="Enter Title "
+                placeholder="Toyota Premio 2020"
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
             </div>
 
             <div>
               <label className="mb-3 block text-black dark:text-white">
-                Model
+                Driving License Front Side
               </label>
               <input
-                type="text"
-                placeholder="Enter Model "
-                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                type="file"
+                className="w-full border-[1.5px] border-stroke bg-transparent  text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Driving License Back Side
+              </label>
+              <input
+                type="file"
+                className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
             </div>
 
             <div>
               <label className="mb-3 block text-black dark:text-white">
-                Brand
+                Car Bluebook / RC Front Side
               </label>
               <input
-                type="text"
-                placeholder="Enter Brand "
-                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                type="file"
+                className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+            <div>
+              <label className="mb-3 block text-black dark:text-white">
+                Car Bluebook / RC Back Side
+              </label>
+              <input
+                type="file"
+                className="w-full border-[1.5px] border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
             </div>
 
             <div>
-              <SelectGroupThree />
-            </div>
-
-            <div>
-              <label className="mb-3 block text-black dark:text-white">
-                Description
-              </label>
-              <textarea
-                rows={6}
-                placeholder="Description"
-                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-              ></textarea>
+              <input
+                type="submit"
+                placeholder="Create Driver"
+                className="rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black"
+              />
             </div>
           </div>
         </div>
